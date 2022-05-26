a7affee5b55835d40918ed85d03e5778b0496fef
dumingstudy <273099274@qq.com>
2022-05-26 22:34:48 +0800

    Gateway网关自定义过滤器

---
+++ springcloud-gateway-9527/src/main/java/com/dm/filter/MyLogGatewayFilter.java
@@ -0,0 +1,34 @@
+package com.dm.filter;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author D&M
+ * @date 2022/5/26
+ * @apiNote
+ */
+@Component
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (StringUtils.isBlank(uname)) {
+            System.out.println("用户名为null，非法");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
